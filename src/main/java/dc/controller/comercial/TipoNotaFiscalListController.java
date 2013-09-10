74f3103ef2623792cd404fc7cc7227e2f52ea0b2
Rafael <rafa834@gmail.com>
2013-09-10 20:55:32 -0300

    UPLOAD ESTOQUE NF-e

---
+++ src/main/java/dc/controller/comercial/TipoNotaFiscalListController.java
@@ -0,0 +1,65 @@
+package dc.controller.comercial;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import dc.entidade.comercial.TipoNotaFiscal;
+import dc.servicos.dao.comercial.TipoNotaFiscalDAO;
+import dc.visao.framework.geral.CRUDFormController;
+import dc.visao.framework.geral.CRUDListController;
+
+public class TipoNotaFiscalListController
+extends
+CRUDListController<TipoNotaFiscal>{
+	
+	@Autowired
+	TipoNotaFiscalDAO dao;
+	
+	@Autowired
+	TipoNotaFiscalFormController formController;
+	
+	@Override
+	protected String[] getColunas() {
+		return new String[] {"modelo"};
+	}
+
+	@Override
+	protected String getTitulo() {
+		return "Tipo Nota Fiscal";
+	}
+	
+	@Override
+	protected CRUDFormController<TipoNotaFiscal> getFormController() {
+		return formController;
+	}
+
+	@Override
+	protected Class<? super TipoNotaFiscal> getEntityClass() {
+		return TipoNotaFiscal.class;
+	}
+
+	@Override
+	public String getViewIdentifier() {
+		return "Tipo Nota Fiscal";
+	}
+
+	@Override
+	protected List<TipoNotaFiscal> pesquisa(String valor) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected List<TipoNotaFiscal> pesquisaDefault() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected boolean deletaEmCascata() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
