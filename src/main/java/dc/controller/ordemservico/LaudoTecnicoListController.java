8e688f0ee1e259300829768f92c8a5c887480fee
sagcec <sag.cec@gmail.com>
2013-09-13 19:40:58 -0300

    Módulo: ORDEMSERVICO. Descrição: Criação das classes CONTROLLER e VIEW.

---
+++ src/main/java/dc/controller/ordemservico/LaudoTecnicoListController.java
@@ -0,0 +1,81 @@
+package dc.controller.ordemservico;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+
+import dc.visao.framework.geral.CRUDFormController;
+import dc.visao.framework.geral.CRUDListController;
+
+/**
+ * 
+ * @author Gutemberg A. Da Silva
+ * 
+ */
+
+@Controller
+@Scope("prototype")
+public class LaudoTecnicoListController extends CRUDListController<Object> {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * DAO'S
+	 */
+
+	/**
+	 * CONTROLLER'S
+	 */
+
+	@Autowired
+	private LaudoTecnicoFormController pController;
+
+	@Override
+	protected String[] getColunas() {
+		return new String[] { "" };
+	}
+
+	@Override
+	protected Class<? super Object> getEntityClass() {
+		return Object.class;
+	}
+
+	@Override
+	protected String getTitulo() {
+		return "Laudo técnico";
+	}
+
+	@Override
+	protected List<Object> pesquisa(String valor) {
+
+		return null;
+	}
+
+	@Override
+	protected CRUDFormController<Object> getFormController() {
+		return this.pController;
+	}
+
+	// Identificador da VIEW, para posterior uso nas urls de navegacao
+	@Override
+	public String getViewIdentifier() {
+		return "ordemservico_laudotecnico_lc";
+	}
+
+	@Override
+	protected boolean deletaEmCascata() {
+		return false;
+	}
+
+	@Override
+	protected List<Object> pesquisaDefault() {
+
+		return null;
+	}
+
+}
--- src/main/java/dc/controller/ordemservico/TesteFormController.java
+++
@@ -1,5 +0,0 @@
-package dc.controller.ordemservico;
-
-public class TesteFormController {
-
-}
