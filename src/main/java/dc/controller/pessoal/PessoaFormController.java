5aa41b54f6cfbc113a1c1e8c47d6e0cba46c04c5
Rafael Roque <rafa834@gmail.com>
2013-09-16 21:46:42 -0300

    Controller e View de Pessoa

---
+++ src/main/java/dc/controller/pessoal/PessoaFormController.java
@@ -0,0 +1,129 @@
+package dc.controller.pessoal;
+
+import java.io.Serializable;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+
+import com.vaadin.ui.Component;
+
+import dc.entidade.framework.Empresa;
+import dc.entidade.geral.Pessoa;
+
+import dc.servicos.dao.pessoal.PessoaDAO;
+import dc.servicos.util.Validator;
+import dc.visao.framework.geral.CRUDFormController;
+import dc.visao.pessoal.PessoaFormView;
+import dc.visao.spring.SecuritySessionProvider;
+
+@Controller
+@Scope("prototype")
+@SuppressWarnings("serial")
+public class PessoaFormController extends CRUDFormController<Pessoa> {
+
+	PessoaFormView subView;
+
+	@Autowired
+	PessoaDAO dao;
+	
+	
+
+	Pessoa currentBean;
+
+	@Override
+	public String getViewIdentifier() {
+		return "PessoaForm";
+	}
+
+	@Override
+	protected boolean validaSalvar() {
+		boolean valido = true;
+		
+		
+		
+		return valido;
+	}
+
+	@Override
+	protected void criarNovoBean() {
+		currentBean = new Pessoa();
+	}
+
+	@Override
+	protected void initSubView() {
+		subView = new PessoaFormView(this);
+
+	}
+
+	@Override
+	protected void carregar(Serializable id) {
+		// TODO Auto-generated method stub
+		currentBean = dao.find((Integer) id);
+	}
+	
+	public Empresa empresaAtual(){
+		return SecuritySessionProvider.getUsuario().getConta().getEmpresa();
+	}
+
+	@Override
+	protected void actionSalvar() {
+		try{
+			dao.saveOrUpdate(currentBean);
+			notifiyFrameworkSaveOK(this.currentBean);
+		}catch(Exception e){
+			mensagemErro("Erro!!");
+			e.printStackTrace();
+		}
+		
+
+	}
+
+	
+
+	@Override
+	protected void quandoNovo() {
+		try{
+			//subView.filPessoaDetalhesSubForm(currentBean.getContagemDetalhes());
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		
+	}
+	
+	
+
+	@Override
+	protected Component getSubView() {
+		return subView;
+	}
+
+	@Override
+	protected String getNome() {
+		return "Contagem Estoque";
+	}
+
+	@Override
+	protected void remover(List<Serializable> ids) {
+	
+		dao.deleteAllByIds(ids);
+
+	}
+	@Override
+	protected void removerEmCascata(List<Serializable> objetos) {
+		System.out.println("");
+
+	}
+	
+	@Override
+	public boolean isFullSized(){
+		return true;
+	}
+	
+
+	
+	
+
+}
+
