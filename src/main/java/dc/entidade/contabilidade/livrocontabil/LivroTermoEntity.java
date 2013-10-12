51f25a4f62dd69bcaadf1692f694f1acb9267228
sagcec <sag.cec@gmail.com>
2013-10-12 18:05:43 -0300

    Módulo: CONTABILIDADE. Descrição: Início das atividades.

---
+++ src/main/java/dc/entidade/contabilidade/livrocontabil/LivroTermoEntity.java
@@ -0,0 +1,57 @@
+package dc.entidade.contabilidade.livrocontabil;
+
+import java.io.Serializable;
+
+import org.apache.commons.lang.builder.ToStringBuilder;
+
+import dc.entidade.framework.AbstractMultiEmpresaModel;
+
+public class LivroTermoEntity extends AbstractMultiEmpresaModel<Integer>
+		implements Serializable {
+
+	/**
+*
+*/
+	private static final long serialVersionUID = 1L;
+
+	private Integer id;
+
+	/**
+	 * REFERENCIA - FK
+	 */
+
+	/**
+	 * REFERENCIA - LIST
+	 */
+
+	/**
+	 * CONSTRUTOR
+	 */
+
+	public LivroTermoEntity() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * GETS AND SETS
+	 */
+
+	@Override
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	/**
+	 * TO STRING
+	 */
+
+	@Override
+	public String toString() {
+		return ToStringBuilder.reflectionToString(this);
+	}
+
+}
