045ce18b8606ba6e203a8196c3dabfe81a3a24a3
WESLEY J <wesley@dotcompany.com.br>
2013-10-15 17:47:23 -0300

    MÓDULOS... CONTINUAÇÃO

---
+++ src/main/java/dc/controller/tabelas/SalarioMinimoListController.java
@@ -0,0 +1,75 @@
+package dc.controller.tabelas;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+
+import dc.entidade.tabelas.SalarioMinimo;
+import dc.servicos.dao.tabelas.SalarioMinimoDAO;
+import dc.visao.framework.geral.CRUDFormController;
+import dc.visao.framework.geral.CRUDListController;
+
+
+
+/**
+*
+* @author Wesley Jr
+/*
+*/
+
+@Controller
+@Scope("prototype")
+public class SalarioMinimoListController extends CRUDListController<SalarioMinimo>{
+
+	@Autowired
+	private SalarioMinimoDAO dao;
+	
+	@Autowired
+	private SalarioMinimoFormController salarioMinimoFormController;
+	
+
+	@Override
+	protected String[] getColunas() {
+		return new String[] {"vigencia","valorMensal","valorDiario","valorHora"};
+	}
+
+	@Override
+	protected Class<? super SalarioMinimo> getEntityClass() {
+		return SalarioMinimo.class;
+	}
+
+
+	@Override
+	protected String getTitulo() {
+		return "Salário Mínimo";
+	}
+
+	@Override
+	protected List<SalarioMinimo> pesquisa(String valor) {
+		return dao.fullTextSearch(valor);
+	}
+	
+
+	@Override
+	protected CRUDFormController<SalarioMinimo> getFormController() {
+		return salarioMinimoFormController;
+	}
+
+	//Identificador da VIEW, para posterior uso nas urls de navegacao
+	@Override
+	public String getViewIdentifier() {
+		return "listaSalarioMinimo";
+	}
+
+	@Override
+	protected boolean deletaEmCascata() {
+		return false;
+	}
+
+	@Override
+	protected List<SalarioMinimo> pesquisaDefault() {
+		return (List<SalarioMinimo>) dao.getAll(getEntityClass());
+	}
+}
---
+++ src/main/java/dc/entidade/tabelas/CodigoGps.java
@@ -0,0 +1,81 @@
+package dc.entidade.tabelas;
+
+import java.io.Serializable;
+
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
+
+import org.apache.lucene.analysis.br.BrazilianAnalyzer;
+import org.hibernate.search.annotations.Analyzer;
+import org.hibernate.search.annotations.Field;
+import org.hibernate.search.annotations.Indexed;
+
+import dc.anotacoes.Caption;
+import dc.entidade.framework.AbstractModel;
+
+/**
+ * 
+ * @author Wesley Jr
+ *
+ */
+
+@Entity
+@Table(name = "codigo_gps")
+@XmlRootElement
+@Indexed
+@Analyzer(impl=BrazilianAnalyzer.class)
+public class CodigoGps extends AbstractModel<Integer> implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@Column(name = "id", nullable = false)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "codigo_gps_id_seq")
+	@SequenceGenerator(name = "codigo_gps_id_seq", sequenceName = "codigo_gps_id_seq", allocationSize = 1, initialValue = 0)
+	@Basic(optional = false)
+	private Integer id;
+	
+	@Field
+	@Caption("Codigo")
+	@Column(name="CODIGO")
+	private Integer codigo;
+	
+	@Field
+	@Caption("Descricao")
+	@Column(name="DESCRICAO")
+	private String descricao;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Integer getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(Integer codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	
+	
+}
