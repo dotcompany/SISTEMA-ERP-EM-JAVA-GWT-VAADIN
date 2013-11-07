b5d715b8b54958bf039759dc18ad1cad398a55a8
Rafael Roque <rafa834@gmail.com>
2013-11-07 01:15:11 -0200

    aajuste

---
+++ src/main/java/dc/controller/financeiro/ParcelaReceberListController.java
@@ -0,0 +1,72 @@
+package dc.controller.financeiro;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+
+import dc.entidade.financeiro.ParcelaReceber;
+import dc.servicos.dao.financeiro.ParcelaReceberDAO;
+import dc.visao.framework.geral.CRUDFormController;
+import dc.visao.framework.geral.CRUDListController;
+
+@Controller
+@Scope("prototype")
+public class ParcelaReceberListController extends CRUDListController<ParcelaReceber> {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Autowired
+	private ParcelaReceberDAO dao;
+
+	@Autowired
+	private ParcelaRecebimentoFormController parcelaRecebimentoFormController;
+
+	@Override
+	protected String[] getColunas() {
+		return new String[] { "contaCaixa", "numeroParcela", "dataEmissao", "dataVencimento", "descontoAte", "valor", "taxaJuro", "valorJuro",
+				"taxaMulta", "valorMulta", "taxaDesconto", "valorDesconto", "emitiuBoleto", "boletoNossoNumero" };
+	}
+
+	@Override
+	protected Class<? super ParcelaReceber> getEntityClass() {
+		return ParcelaReceber.class;
+	}
+
+	@Override
+	protected String getTitulo() {
+		return "Parcela à Receber";
+	}
+
+	@Override
+	protected List<ParcelaReceber> pesquisa(String valor) {
+		return dao.fullTextSearch(valor);
+	}
+
+	@Override
+	protected CRUDFormController<ParcelaReceber> getFormController() {
+		return parcelaRecebimentoFormController;
+	}
+
+	// Identificador da VIEW, para posterior uso nas urls de navegacao
+	@Override
+	public String getViewIdentifier() {
+		return "listaParcelasReceber";
+	}
+
+	@Override
+	protected boolean deletaEmCascata() {
+		return false;
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	protected List<ParcelaReceber> pesquisaDefault() {
+		return (List<ParcelaReceber>) dao.getAll(getEntityClass());
+	}
+
+}
