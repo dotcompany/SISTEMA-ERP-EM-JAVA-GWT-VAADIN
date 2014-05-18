55fcf27affd0dd39fdfcec260ce5a15a0c53937a
sgulless <sgulless@sgullesss-mbp.home>
2014-05-18 11:09:53 -0300

    Módulo: NFE. Descrição: Ajustes para realização do cálculo.

---
+++ src/main/java/dc/controller/nfe/NfeDetalheImpostoCofinsListController.java
@@ -0,0 +1,108 @@
+package dc.controller.nfe;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+
+import dc.control.util.ClasseUtil;
+import dc.entidade.nfe.NfeDetalheImpostoCofinsEntity;
+import dc.servicos.dao.nfe.NfeDetalheImpostoCofinsDAO;
+import dc.visao.framework.geral.CRUDFormController;
+import dc.visao.framework.geral.CRUDListController;
+
+/**
+ * 
+ * @author Gutemberg A. Da Silva
+ * 
+ */
+
+@Controller
+@Scope("prototype")
+public class NfeDetalheImpostoCofinsListController extends
+		CRUDListController<NfeDetalheImpostoCofinsEntity> {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * DAO'S
+	 */
+
+	@Autowired
+	private NfeDetalheImpostoCofinsDAO pDAO;
+
+	/**
+	 * CONTROLLER'S
+	 */
+
+	@Autowired
+	private NfeDetalheImpostoCofinsFormController pController;
+
+	@Override
+	public String[] getColunas() {
+		return new String[] { "codigoNumerico", "naturezaOperacao",
+				"indicadorFormaPagamento" };
+	}
+
+	@Override
+	public Class<? super NfeDetalheImpostoCofinsEntity> getEntityClass() {
+		return NfeDetalheImpostoCofinsEntity.class;
+	}
+
+	@Override
+	protected String getTitulo() {
+		return "Produto / serviço";
+	}
+
+	@Override
+	protected List<NfeDetalheImpostoCofinsEntity> pesquisa(String valor) {
+		try {
+			List<NfeDetalheImpostoCofinsEntity> auxLista = this.pDAO
+					.procuraNomeContendo(valor);
+
+			return auxLista;
+		} catch (Exception e) {
+			e.printStackTrace();
+
+			return new ArrayList<NfeDetalheImpostoCofinsEntity>();
+		}
+	}
+
+	@Override
+	protected CRUDFormController<NfeDetalheImpostoCofinsEntity> getFormController() {
+		return this.pController;
+	}
+
+	// Identificador da VIEW, para posterior uso nas urls de navegacao
+	@Override
+	public String getViewIdentifier() {
+		String sUrl = ClasseUtil.getUrl(this);
+
+		return sUrl;
+	}
+
+	@Override
+	protected boolean deletaEmCascata() {
+		return false;
+	}
+
+	@Override
+	protected List<NfeDetalheImpostoCofinsEntity> pesquisaDefault() {
+		try {
+			List<NfeDetalheImpostoCofinsEntity> auxLista = this.pDAO
+					.listarTodos();
+
+			return auxLista;
+		} catch (Exception e) {
+			e.printStackTrace();
+
+			return new ArrayList<NfeDetalheImpostoCofinsEntity>();
+		}
+	}
+
+}
